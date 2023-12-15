18f130eb20a70114d992b4c9adc09ad08891db26
fred <fred.shone@me.com>
2023-12-15 17:19:21 +0000

    cov

--- tests/test_report.py
+++ tests/test_report.py
@@ -1,14 +1,13 @@
 from numpy import array
 from pandas import DataFrame, Series
 
-from caveat.describe.features import average
 from caveat.distances.scalar import mae
 from caveat.features.times import start_times_by_act
-from caveat.report import describe_feature, extract_default, score_features
+from caveat.report import extract_default, report, score_features
 
 
 def test_describe_feature():
-    observed = DataFrame(
+    DataFrame(
         [
             {"pid": 0, "act": "home", "start": 0},
             {"pid": 0, "act": "work", "start": 2},
@@ -17,37 +16,6 @@ def test_describe_feature():
             {"pid": 1, "act": "work", "start": 1},
         ]
     )
-    # ys = {
-    #     "y1": DataFrame(
-    #         [
-    #             {"pid": 0, "act": "home", "start": 0},
-    #             {"pid": 0, "act": "work", "start": 2},
-    #             {"pid": 0, "act": "home", "start": 6},
-    #             {"pid": 1, "act": "home", "start": 0},
-    #             {"pid": 1, "act": "work", "start": 1},
-    #         ]
-    #     ),
-    #     "y2": DataFrame(
-    #         [
-    #             {"pid": 0, "act": "home", "start": 0},
-    #             {"pid": 0, "act": "work", "start": 2},
-    #             {"pid": 0, "act": "home", "start": 6},
-    #             {"pid": 1, "act": "home", "start": 0},
-    #             {"pid": 1, "act": "work", "start": 3},
-    #         ]
-    #     ),
-    # }
-    # data = {
-    #     "observed": {"home": 2.0, "work": 1.5},
-    #     "y1": {"home": 2.0, "work": 1.5},
-    #     "y1 delta": {"home": 0.0, "work": 0.0},
-    #     "y2": {"home": 2.0, "work": 2.5},
-    #     "y2 delta": {"home": 0.0, "work": 1.0},
-    # }
-    expected = Series({"home": 2.0, "work": 1.5}, name="test")
-    start_times = start_times_by_act(observed)
-    result = describe_feature("test", start_times, average)
-    assert result.equals(expected)
 
 
 def test_create_default():
@@ -113,3 +81,25 @@ def test_score_features_with_default():
         "test", x, y, mae, (array([0]), array([1]))
     ).sort_index()
     assert result.equals(expected)
+
+
+def test_report():
+    observed = DataFrame(
+        [
+            {"pid": 0, "act": "home", "start": 0, "end": 6, "duration": 6},
+            {"pid": 0, "act": "work", "start": 6, "end": 14, "duration": 8},
+            {"pid": 0, "act": "home", "start": 14, "end": 24, "duration": 10},
+            {"pid": 1, "act": "home", "start": 0, "end": 10, "duration": 10},
+            {"pid": 1, "act": "work", "start": 10, "end": 24, "duration": 14},
+        ]
+    )
+    y = DataFrame(
+        [
+            {"pid": 0, "act": "home", "start": 0, "end": 6, "duration": 6},
+            {"pid": 0, "act": "work", "start": 6, "end": 14, "duration": 8},
+            {"pid": 0, "act": "home", "start": 14, "end": 24, "duration": 10},
+            {"pid": 1, "act": "home", "start": 0, "end": 10, "duration": 10},
+            {"pid": 1, "act": "work", "start": 10, "end": 24, "duration": 14},
+        ]
+    )
+    report(observed, {"y": y}, None)
