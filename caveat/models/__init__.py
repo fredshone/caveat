abf3060f7dde59e27732624685f75c6208f45dd6
Fred Shone <26383933+fredshone@users.noreply.github.com>
2024-04-29 18:41:28 +0100

    renamed module

--- caveat/models/__init__.py
+++ caveat/models/__init__.py
@@ -3,9 +3,9 @@ from .discrete.embed_conv import Conv
 from .discrete.lstm_discrete import LSTM_Discrete
 from .discrete.transformer_discrete import AttentionDiscrete
 from .embed import CustomDurationEmbedding
-from .sequence.conditional_LSTM import ConditionalLSTM
-from .sequence.conditional_VAE_LSTM import CVAE_LSTM
-from .sequence.VAE_LSTM import VAE_LSTM
+from .sequence.conditional_lstm import ConditionalLSTM
+from .sequence.conditional_vae_lstm import CVAE_LSTM
+from .sequence.vae_lstm import VAE_LSTM
 
 library = {
     "VAE_Conv_Discrete": Conv,
