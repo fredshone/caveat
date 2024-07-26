cc345aa8b0d3946a86875fcf68530d4f67d2411f
Fred Shone <26383933+fredshone@users.noreply.github.com>
2024-07-26 16:05:32 +0100

    norm latent layer nmodel

--- caveat/models/__init__.py
+++ caveat/models/__init__.py
@@ -12,21 +12,22 @@ from .sequence.auto_sequence_lstm import AutoSeqLSTM
 from .sequence.cond_sequence_lstm import CondSeqLSTM
 from .sequence.cvae_sequence_lstm import CVAESeqLSTM
 from .sequence.cvae_sequence_lstm_2 import CVAESeqLSTM2
+from .sequence.cvae_sequence_lstm_3 import CVAESeqLSTM3
 from .sequence.vae_sequence_lstm import VAESeqLSTM
 
 library = {
-    "Koushik": CondDiscLSTM,
-    "CondDiscLSTM": CondDiscLSTM,
-    "CondDiscConv": CondDiscConv,
-    "CondSeqLSTM": CondSeqLSTM,
-    "AutoDiscLSTM": AutoDiscLSTM,
+    "CondDiscLSTM": CondDiscLSTM,  # lstm unit given attributes as input at every step
+    "CondDiscConv": CondDiscConv,  # similar to koushik but with CNN
+    "CondSeqLSTM": CondSeqLSTM,  # lstm unit given attributes as input at first step
+    "AutoDiscLSTM": AutoDiscLSTM,  # lstm unit input is previous output
     "AutoSeqLSTM": AutoSeqLSTM,
     "VAEDiscConv": VAEDiscConv,
     "VAESeqLSTM": VAESeqLSTM,
     "VAEDiscLSTM": VAEDiscLSTM,
     "VAEDiscTrans": VAEDiscTrans,
-    "CVAESeqLSTM": CVAESeqLSTM,
-    "CVAESeqLSTM2": CVAESeqLSTM2,
+    "CVAESeqLSTM": CVAESeqLSTM,  # attributes at decoder only
+    "CVAESeqLSTM2": CVAESeqLSTM2,  # attributes at encoder and decoder
+    "CVAESeqLSTM3": CVAESeqLSTM3,  # normed decoder attributes size
     "Seq2SeqLSTM": Seq2SeqLSTM,
     "Seq2ScoreLSTM": Seq2ScoreLSTM,
 }
